ad3a8918e56ee9266ca26a55f886a1b718e2e869
SK207_04 <SK207_04@K207_04.carrollcc.edu>
2016-09-19 20:09:04 -0400

    Added scanner and question

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -1,9 +1,15 @@
+import java.util.Scanner;
 
 public class HelloWorld {
 
 	public static void main(String[] args) 
 	{
+		Scanner keyboard = new Scanner(System.in);
+		
 		System.out.println("Hello World!!!!!");
+		
+		System.out.println("Enter Name: ");
+		String name = keyboard.nextLine();
 		
 		System.out.println("Goodbye cruel world!");
 	}
